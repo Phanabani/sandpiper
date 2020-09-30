046e99fb14dec13b35bd8640d7a434176a590481
Hawkpath <hawkpathas@gmail.com>
2020-09-30 03:08:19 -0400

    Added logging file handlers rather than outputting to sysout.

--- sandpiper/__main__.py
+++ sandpiper/__main__.py
@@ -1,19 +1,30 @@
 import logging
+from logging.handlers import TimedRotatingFileHandler
 from pathlib import Path
-import sys
 
 from . import Sandpiper, Config
 
-logger = logging.getLogger('sandpiper')
+LOGS_PATH = Path(__file__).parent / 'logs'
+formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s')
 
+# Sandpiper logging
+logger = logging.getLogger('sandpiper')
 logger.setLevel(logging.DEBUG)
-handler = logging.StreamHandler(sys.stdout)
-handler.setFormatter(logging.Formatter(
-    '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+handler = TimedRotatingFileHandler(LOGS_PATH / 'sandpiper.log',
+                                   when='midnight', backupCount=7)
+handler.setFormatter(formatter)
 logger.addHandler(handler)
 
+# Discord logging
+logger = logging.getLogger('discord')
+logger.setLevel(logging.WARNING)
+handler = TimedRotatingFileHandler(LOGS_PATH / 'sandpiper.log',
+                                   when='midnight', backupCount=7)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+# Run bot
 config_path = Path(__file__).parent / 'config.json'
 bot_token, config = Config.load_json(config_path)
-
 sandpiper = Sandpiper(config)
 sandpiper.run(bot_token)
