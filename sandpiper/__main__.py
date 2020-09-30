3224d812a5ed124e8724782f22586211b1438ee0
Hawkpath <hawkpathas@gmail.com>
2020-09-30 04:51:44 -0400

    Overhauled config and config loader to be more structured and error-resistant.

--- sandpiper/__main__.py
+++ sandpiper/__main__.py
@@ -1,30 +1,22 @@
 import logging
-from logging.handlers import TimedRotatingFileHandler
 from pathlib import Path
 
 from . import Sandpiper, Config
 
-LOGS_PATH = Path(__file__).parent / 'logs'
-formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s')
+# Load config
+config_path = Path(__file__).parent / 'config.json'
+bot_token, config = Config.load_json(config_path)
 
 # Sandpiper logging
 logger = logging.getLogger('sandpiper')
 logger.setLevel(logging.DEBUG)
-handler = TimedRotatingFileHandler(LOGS_PATH / 'sandpiper.log',
-                                   when='midnight', backupCount=7)
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+logger.addHandler(config.logging.handler)
 
 # Discord logging
 logger = logging.getLogger('discord')
 logger.setLevel(logging.WARNING)
-handler = TimedRotatingFileHandler(LOGS_PATH / 'sandpiper.log',
-                                   when='midnight', backupCount=7)
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+logger.addHandler(config.logging.handler)
 
 # Run bot
-config_path = Path(__file__).parent / 'config.json'
-bot_token, config = Config.load_json(config_path)
-sandpiper = Sandpiper(config)
+sandpiper = Sandpiper(config.bot)
 sandpiper.run(bot_token)
--- sandpiper/config.py
+++ sandpiper/config.py
@@ -1,34 +1,139 @@
 from __future__ import annotations
 import json
+import logging
+from logging.handlers import TimedRotatingFileHandler
 from pathlib import Path
 from typing import Any, Dict, Tuple, Union
 
-DEFAULT_COMMAND_PREFIX = '!piper '
-DEFAULT_DESCRIPTION = 'A bot that makes it easier to communicate with friends around the world.'
+DEFAULTS = {
+  "bot": {
+    "command_prefix": "!piper ",
+    "description": "A bot that makes it easier to communicate with friends around the world."
+  },
+
+  "logging": {
+    "output_file": "./logs/sandpiper.log",
+    "when": "midnight",
+    "interval": 1,
+    "backup_count": 7,
+    "format": "%(asctime)s:%(levelname)s:%(name)s: %(message)s"
+  }
+}
+
+
+class ConfigError(Exception):
+    pass
+
+
+def get_default(config: Dict[str, Any], category: str, key: str):
+    try:
+        return config[category][key]
+    except KeyError:
+        return DEFAULTS[category][key]
 
 
 class Config:
 
-    __slots__ = ['command_prefix', 'description']
+    __slots__ = ['bot', 'logging']
 
-    def __init__(self, command_prefix: str, description: str):
-        self.command_prefix = command_prefix
-        self.description = description
+    class _Bot:
+
+        __slots__ = ['command_prefix', 'description']
+
+        command_prefix: str
+        description: str
+
+        def __init__(self, config: Dict[str, Any]):
+            """Parse bot-specific config"""
+
+            self.command_prefix = get_default(config, 'bot', 'command_prefix')
+            if not isinstance(self.command_prefix, str):
+                raise ConfigError('bot.command_prefix must be a string')
+
+            self.description = get_default(config, 'bot', 'description')
+            if not isinstance(self.description, str):
+                raise ConfigError('bot.description must be a string')
+
+    class _Logging:
+
+        __slots__ = ['output_path', 'when', 'interval', 'backup_count',
+                     'format', 'formatter', 'handler']
+
+        output_path: Path
+        when: str
+        interval: int
+        backup_count: int
+        format: str
+        formatter: logging.Formatter
+        handler: TimedRotatingFileHandler
+
+        _allowed_whens = ('S', 'M', 'H', 'D', 'midnight')
+
+        def __init__(self, config: Dict[str, Any]):
+            """Parse logging-specific config"""
+
+            output_file = get_default(config, 'logging', 'output_file')
+            if not isinstance(output_file, str):
+                raise ConfigError('logging.output_file must be a string')
+            output_file = Path(output_file)
+            if not output_file.is_absolute():
+                output_file = Path(__file__).parent / output_file
+            if not output_file.parent.exists():
+                raise ConfigError(f"logging.output_file's parent directory "
+                                  f"does not exist ({output_file.parent})")
+            self.output_path = output_file
+
+            self.when = get_default(config, 'logging', 'when')
+            if self.when not in self._allowed_whens:
+                raise ConfigError(f"logging.when must be one of "
+                                  f"{self._allowed_whens!r}")
+
+            self.interval = get_default(config, 'logging', 'interval')
+            if not isinstance(self.interval, int) or self.interval < 1:
+                raise ConfigError('logging.interval must be an integer greater '
+                                  'than 0')
+
+            self.backup_count = get_default(config, 'logging', 'backup_count')
+            if not isinstance(self.backup_count, int) or self.backup_count < 0:
+                raise ConfigError('logging.backup_count must be an integer '
+                                  'greater than or equal to 0')
+
+            self.format = get_default(config, 'logging', 'format')
+            if not isinstance(self.format, str):
+                raise ConfigError('logging.format must be a string')
+
+            self.formatter = logging.Formatter(self.format)
+            self.handler = TimedRotatingFileHandler(
+                filename=self.output_path,
+                when=self.when,
+                interval=self.interval,
+                backupCount=self.backup_count,
+            )
+            self.handler.setFormatter(self.formatter)
+
+    def __init__(self, config: Dict[str, Any]):
+        """Parse config"""
+        self.bot = self._Bot(config)
+        self.logging = self._Logging(config)
 
     @classmethod
     def load_json(cls, config_path: Union[Path, str]) -> Tuple[str, Config]:
         """
-        Load bot config info from a json file.
+        Load bot config from a json file.
 
         :param config_path: Path to the json file
         :returns: A tuple of (bot_token, config). For security reasons, the
             bot token isn't loaded into the config object.
         """
+
         with open(config_path) as f:
             config_json: Dict[str, Any] = json.load(f)
+
+        if 'bot_token' not in config_json:
+            raise ConfigError('bot_token missing')
         bot_token = config_json['bot_token']
-        config = Config(
-            command_prefix=config_json.get('command_prefix', DEFAULT_COMMAND_PREFIX),
-            description=config_json.get('description', DEFAULT_DESCRIPTION)
-        )
+        # Delete bot_token from dict just in case
+        del config_json['bot_token']
+
+        config = Config(config_json)
         return bot_token, config
