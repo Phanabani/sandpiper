3b249359b96a8ece6af3b4b1b80ea7fd3aaeaca4
Hawkpath <hawkpathas@gmail.com>
2020-09-30 01:28:51 -0400

    Added Windows platform check for the expired certificates fix.

--- sandpiper/__init__.py
+++ sandpiper/__init__.py
@@ -1,6 +1,9 @@
-import certifi
-import os
-os.environ['SSL_CERT_FILE'] = certifi.where()
+import sys
+if sys.platform == 'win32':
+    # Temporary fix for expired certificates on Windows
+    import certifi
+    import os
+    os.environ['SSL_CERT_FILE'] = certifi.where()
 
 from .config import Config
 from .sandpiper import Sandpiper
