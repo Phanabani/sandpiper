94a1bd1648cc521bebd43aa1391db37ca1494d9b
Hawkpath <hawkpathas@gmail.com>
2020-10-03 14:59:43 -0400

    Added database abstract methods.

--- sandpiper/user_data/database.py
+++ sandpiper/user_data/database.py
@@ -1,5 +1,39 @@
 from abc import ABCMeta, abstractmethod
+from datetime import datetime
+
+from pytz import timezone
 
 
 class Database(metaclass=ABCMeta):
-    pass
+
+    @abstractmethod
+    def get_preferred_name(self, user_id: int) -> str:
+        pass
+
+    @abstractmethod
+    def set_preferred_name(self, user_id: int, new_name: str):
+        pass
+
+    @abstractmethod
+    def get_pronouns(self, user_id: int) -> str:
+        pass
+
+    @abstractmethod
+    def set_pronouns(self, user_id: int, new_pronouns: str):
+        pass
+
+    @abstractmethod
+    def get_birthday(self, user_id: int) -> datetime:
+        pass
+
+    @abstractmethod
+    def set_birthday(self, user_id: int, new_birthday: datetime):
+        pass
+
+    @abstractmethod
+    def get_timezone(self, user_id: int) -> timezone:
+        pass
+
+    @abstractmethod
+    def set_timezone(self, user_id: int, new_timezone: timezone):
+        pass
