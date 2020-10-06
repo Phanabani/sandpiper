5ec1bf8b7a0200208d65dd65112e76fd68199482
Hawkpath <hawkpathas@gmail.com>
2020-10-06 06:16:45 -0400

    Added database adapter initialization. The adapter type should be obtained from the config in the future.

--- sandpiper/user_info/__init__.py
+++ sandpiper/user_info/__init__.py
@@ -1,11 +1,18 @@
+from pathlib import Path
+
 from discord.ext.commands import Bot
 
 from .cog import UserData
+from .database_sqlite import DatabaseSQLite
+
+DB_FILE = Path(__file__).parent.parent / 'sandpiper.db'
 
 
 def setup(bot: Bot):
     # TODO instantiate database adapter here?
-    bot.add_cog(UserData(bot))
+    user_data = UserData(bot)
+    user_data.set_database_adapter(DatabaseSQLite(DB_FILE))
+    bot.add_cog(user_data)
 
 
 def teardown(bot: Bot):
