7ed1a98e70db4eb35a20b2b67a4bad78717087b0
Hawkpath <hawkpathas@gmail.com>
2020-10-08 04:50:59 -0400

    Added client on_error handler.

--- sandpiper/sandpiper.py
+++ sandpiper/sandpiper.py
@@ -30,3 +30,11 @@ class Sandpiper(Bot):
 
     async def on_ready(self):
         logger.info('Sandpiper client started')
+
+    async def on_error(self, event_method: str, *args, **kwargs):
+        if event_method == 'on_message':
+            msg: discord.Message = args[0]
+            logger.error(f'Unhandled in on_message (content: "{msg.content}" author: {msg.author})')
+        else:
+            logger.error(f"Unhandled in {event_method} (args: {args} kwargs: {kwargs})")
+        await super().on_error(event_method, *args, **kwargs)
