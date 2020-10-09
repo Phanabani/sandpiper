2fc8458f68198e79ff8b8b3198d16d37bfbbc4a4
Hawkpath <hawkpathas@gmail.com>
2020-10-09 00:30:56 -0400

    Extracted common get/set operations into methods. Added error handling.

--- sandpiper/user_info/database_sqlite.py
+++ sandpiper/user_info/database_sqlite.py
@@ -1,14 +1,17 @@
 import datetime
+import logging
 from pathlib import Path
 import sqlite3
-from typing import Optional, Union
+from typing import Any, NoReturn, Optional, Union
 
 import pytz
 
-from .database import Database
+from .database import Database, DatabaseError
 from .enums import PrivacyType
 
 __all__ = ['DatabaseSQLite']
+
+logger = logging.getLogger('sandpiper.user_data.database_sqlite')
 
 DEFAULT_PRIVACY = PrivacyType.PRIVATE
 
@@ -29,7 +32,6 @@ sqlite3.register_converter('timezone', convert_timezone)
 class DatabaseSQLite(Database):
 
     _con: Optional[sqlite3.Connection] = None
-    _cur: Optional[sqlite3.Cursor] = None
     db_path: Union[str, Path]
 
     def __init__(self, db_path: Union[str, Path]):
@@ -40,199 +42,142 @@ class DatabaseSQLite(Database):
     def connect(self):
         self._con = sqlite3.connect(
             self.db_path, detect_types=sqlite3.PARSE_DECLTYPES)
-        self._cur = self._con.cursor()
 
     def disconnect(self):
         self._con.close()
         self._con = None
-        self._cur = None
 
     def connected(self):
         return self._con is not None
 
     def create_table(self):
-        self._cur.execute(
-            """
-            CREATE TABLE IF NOT EXISTS user_info (
-                user_id INTEGER PRIMARY KEY UNIQUE, 
-                preferred_name TEXT, 
-                pronouns TEXT, 
-                birthday DATE, 
-                timezone TIMEZONE, 
-                privacy_preferred_name TINYINT, 
-                privacy_pronouns TINYINT, 
-                privacy_birthday TINYINT, 
-                privacy_age TINYINT, 
-                privacy_timezone TINYINT
-            )
-            """
-        )
+        try:
+            with self._con:
+                self._con.execute("""
+                    CREATE TABLE IF NOT EXISTS user_info (
+                        user_id INTEGER PRIMARY KEY UNIQUE, 
+                        preferred_name TEXT, 
+                        pronouns TEXT, 
+                        birthday DATE, 
+                        timezone TIMEZONE, 
+                        privacy_preferred_name TINYINT, 
+                        privacy_pronouns TINYINT, 
+                        privacy_birthday TINYINT, 
+                        privacy_age TINYINT, 
+                        privacy_timezone TINYINT
+                    )
+                """)
+        except sqlite3.Error:
+            logger.error('Failed to create table', exc_info=True)
 
     def clear_data(self, user_id: int):
-        self._cur.execute(
-            'DELETE FROM user_info WHERE user_id = ?',
-            (user_id,)
-        )
+        try:
+            with self._con:
+                self._con.execute(
+                    'DELETE FROM user_info WHERE user_id = ?',
+                    (user_id,)
+                )
+        except sqlite3.Error:
+            logger.error(f'Failed to delete row (user_id={user_id})',
+                         exc_info=True)
+            raise DatabaseError('Failed to delete user data')
+
+    # Getter/setter helpers
+
+    def _do_execute_get(self, col_name: str, user_id: int,
+                        default: Any = None) -> Optional[Any]:
+        stmt = f'SELECT {col_name} FROM user_info WHERE user_id = ?'
+        try:
+            with self._con:
+                result = self._con.execute(stmt, (user_id,)).fetchone()
+        except sqlite3.Error:
+            logger.error(f'Failed to get value (column={col_name!r} '
+                         f'user_id={user_id})',
+                         exc_info=True)
+            raise DatabaseError('Failed to get value')
+        if result is None or result[0] is None:
+            return default
+        return result[0]
+
+    def _do_execute_set(self, col_name: str, user_id: int,
+                        new_value: Any) -> NoReturn:
+        stmt = f'''
+            INSERT INTO user_info(user_id, {col_name})
+            VALUES (:user_id, :new_value)
+            ON CONFLICT (user_id) DO
+            UPDATE SET {col_name} = :new_value
+        '''
+        args = {'user_id': user_id, 'new_value': new_value}
+        try:
+            with self._con:
+                self._con.execute(stmt, args)
+        except sqlite3.Error:
+            logger.error(f'Failed to set value (column={col_name!r} '
+                         f'user_id={user_id} new_value={new_value!r})',
+                         exc_info=True)
+            raise DatabaseError('Failed to set value')
 
     # Preferred name
 
     def get_preferred_name(self, user_id: int) -> Optional[str]:
-        stmt = 'SELECT preferred_name FROM user_info WHERE user_id = ?'
-        result = self._cur.execute(stmt, (user_id,)).fetchone()
-        if result is None:
-            return None
-        return result[0]
+        return self._do_execute_get('preferred_name', user_id)
 
     def set_preferred_name(self, user_id: int, new_preferred_name: str):
-        stmt = '''
-            INSERT INTO user_info(user_id, preferred_name)
-            VALUES (:user_id, :new_preferred_name)
-            ON CONFLICT (user_id) DO
-            UPDATE SET preferred_name = :new_preferred_name
-        '''
-        args = {'user_id': user_id, 'new_preferred_name': new_preferred_name}
-        self._cur.execute(stmt, args)
+        self._do_execute_set('preferred_name', user_id, new_preferred_name)
 
     def get_privacy_preferred_name(self, user_id: int) -> int:
-        stmt = 'SELECT privacy_preferred_name FROM user_info WHERE user_id = ?'
-        result = self._cur.execute(stmt, (user_id,)).fetchone()
-        if result is None or result[0] is None:
-            return DEFAULT_PRIVACY
-        return result[0]
+        return self._do_execute_get(
+            'privacy_preferred_name', user_id, DEFAULT_PRIVACY)
 
     def set_privacy_preferred_name(self, user_id: int, new_privacy: PrivacyType):
-        stmt = '''
-            INSERT INTO user_info(user_id, privacy_preferred_name)
-            VALUES (:user_id, :new_privacy)
-            ON CONFLICT (user_id) DO
-            UPDATE SET privacy_preferred_name = :new_privacy
-        '''
-        args = {'user_id': user_id, 'new_privacy': new_privacy}
-        self._cur.execute(stmt, args)
+        self._do_execute_set('privacy_preferred_name', user_id, new_privacy)
 
     # Pronouns
 
     def get_pronouns(self, user_id: int) -> Optional[str]:
-        stmt = 'SELECT pronouns FROM user_info WHERE user_id = ?'
-        result = self._cur.execute(stmt, (user_id,)).fetchone()
-        if result is None:
-            return None
-        return result[0]
+        return self._do_execute_get('pronouns', user_id)
 
     def set_pronouns(self, user_id: int, new_pronouns: str):
-        stmt = '''
-            INSERT INTO user_info(user_id, pronouns)
-            VALUES (:user_id, :new_pronouns)
-            ON CONFLICT (user_id) DO
-            UPDATE SET pronouns = :new_pronouns
-        '''
-        args = {'user_id': user_id, 'new_pronouns': new_pronouns}
-        self._cur.execute(stmt, args)
+        self._do_execute_set('pronouns', user_id, new_pronouns)
 
     def get_privacy_pronouns(self, user_id: int) -> int:
-        stmt = 'SELECT privacy_pronouns FROM user_info WHERE user_id = ?'
-        result = self._cur.execute(stmt, (user_id,)).fetchone()
-        if result is None or result[0] is None:
-            return DEFAULT_PRIVACY
-        return result[0]
+        return self._do_execute_get('privacy_pronouns', user_id, DEFAULT_PRIVACY)
 
     def set_privacy_pronouns(self, user_id: int, new_privacy: PrivacyType):
-        stmt = '''
-            INSERT INTO user_info(user_id, privacy_pronouns)
-            VALUES (:user_id, :new_privacy)
-            ON CONFLICT (user_id) DO
-            UPDATE SET privacy_pronouns = :new_privacy
-        '''
-        args = {'user_id': user_id, 'new_privacy': new_privacy}
-        self._cur.execute(stmt, args)
+        self._do_execute_set('privacy_pronouns', user_id, new_privacy)
 
     # Birthday
 
     def get_birthday(self, user_id: int) -> Optional[datetime.date]:
-        stmt = 'SELECT birthday FROM user_info WHERE user_id = ?'
-        result = self._cur.execute(stmt, (user_id,)).fetchone()
-        if result is None:
-            return None
-        return result[0]
+        return self._do_execute_get('birthday', user_id)
 
     def set_birthday(self, user_id: int, new_birthday: datetime.date):
-        stmt = '''
-            INSERT INTO user_info(user_id, birthday)
-            VALUES (:user_id, :new_birthday)
-            ON CONFLICT (user_id) DO
-            UPDATE SET birthday = :new_birthday
-        '''
-        args = {'user_id': user_id, 'new_birthday': new_birthday}
-        self._cur.execute(stmt, args)
+        self._do_execute_set('birthday', user_id, new_birthday)
 
     def get_privacy_birthday(self, user_id: int) -> int:
-        stmt = 'SELECT privacy_birthday FROM user_info WHERE user_id = ?'
-        result = self._cur.execute(stmt, (user_id,)).fetchone()
-        if result is None or result[0] is None:
-            return DEFAULT_PRIVACY
-        return result[0]
+        return self._do_execute_get('privacy_birthday', user_id, DEFAULT_PRIVACY)
 
     def set_privacy_birthday(self, user_id: int, new_privacy: PrivacyType):
-        stmt = '''
-            INSERT INTO user_info(user_id, privacy_birthday)
-            VALUES (:user_id, :new_privacy)
-            ON CONFLICT (user_id) DO
-            UPDATE SET privacy_birthday = :new_privacy
-        '''
-        args = {'user_id': user_id, 'new_privacy': new_privacy}
-        self._cur.execute(stmt, args)
+        self._do_execute_set('privacy_birthday', user_id, new_privacy)
 
     # Timezone
 
     def get_timezone(self, user_id: int) -> Optional[pytz.tzinfo.BaseTzInfo]:
-        stmt = 'SELECT timezone FROM user_info WHERE user_id = ?'
-        result = self._cur.execute(stmt, (user_id,)).fetchone()
-        if result is None:
-            return None
-        return result[0]
+        return self._do_execute_get('timezone', user_id)
 
     def set_timezone(self, user_id: int, new_timezone: pytz.tzinfo.BaseTzInfo):
-        stmt = '''
-            INSERT INTO user_info(user_id, timezone)
-            VALUES (:user_id, :new_timezone)
-            ON CONFLICT (user_id) DO
-            UPDATE SET timezone = :new_timezone
-        '''
-        args = {'user_id': user_id, 'new_timezone': new_timezone}
-        self._cur.execute(stmt, args)
+        self._do_execute_set('timezone', user_id, new_timezone)
 
     def get_privacy_timezone(self, user_id: int) -> int:
-        stmt = 'SELECT privacy_timezone FROM user_info WHERE user_id = ?'
-        result = self._cur.execute(stmt, (user_id,)).fetchone()
-        if result is None or result[0] is None:
-            return DEFAULT_PRIVACY
-        return result[0]
+        return self._do_execute_get('privacy_timezone', user_id, DEFAULT_PRIVACY)
 
     def set_privacy_timezone(self, user_id: int, new_privacy: PrivacyType):
-        stmt = '''
-            INSERT INTO user_info(user_id, privacy_birthday)
-            VALUES (:user_id, :new_privacy)
-            ON CONFLICT (user_id) DO
-            UPDATE SET privacy_birthday = :new_privacy
-        '''
-        args = {'user_id': user_id, 'new_privacy': new_privacy}
-        self._cur.execute(stmt, args)
+        self._do_execute_set('privacy_timezone', user_id, new_privacy)
 
     # Age
 
     def get_privacy_age(self, user_id: int) -> int:
-        stmt = 'SELECT privacy_age FROM user_info WHERE user_id = ?'
-        result = self._cur.execute(stmt, (user_id,)).fetchone()
-        if result is None or result[0] is None:
-            return DEFAULT_PRIVACY
-        return result[0]
+        return self._do_execute_get('privacy_age', user_id, DEFAULT_PRIVACY)
 
     def set_privacy_age(self, user_id: int, new_privacy: PrivacyType):
-        stmt = '''
-            INSERT INTO user_info(user_id, privacy_age)
-            VALUES (:user_id, :new_privacy)
-            ON CONFLICT (user_id) DO
-            UPDATE SET privacy_age = :new_privacy
-        '''
-        args = {'user_id': user_id, 'new_privacy': new_privacy}
-        self._cur.execute(stmt, args)
+        self._do_execute_set('privacy_age', user_id, new_privacy)
