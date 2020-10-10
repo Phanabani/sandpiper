96dc0d3d6e29100a18690fce9bab77cedd7bb789
Hawkpath <hawkpathas@gmail.com>
2020-10-10 05:18:39 -0400

    Added documentation for embed methods.

--- sandpiper/common/embeds.py
+++ sandpiper/common/embeds.py
@@ -9,18 +9,36 @@ class Embeds:
 
     @classmethod
     async def info(cls, messageable: discord.abc.Messageable, message: str):
+        """
+        Sends an info embed.
+
+        :param messageable: A messageable interface to send the embed to
+        :param message: The info message
+        """
         embed = discord.Embed(
             title='Info', description=message, color=cls.INFO_COLOR)
         await messageable.send(embed=embed)
 
     @classmethod
     async def success(cls, messageable: discord.abc.Messageable, message: str):
+        """
+        Sends a success embed.
+
+        :param messageable: A messageable interface to send the embed to
+        :param message: The success message
+        """
         embed = discord.Embed(
             title='Success', description=message, color=cls.SUCCESS_COLOR)
         await messageable.send(embed=embed)
 
     @classmethod
-    async def error(cls, messageable: discord.abc.Messageable, reason: str):
+    async def error(cls, messageable: discord.abc.Messageable, message: str):
+        """
+        Sends an error embed.
+
+        :param messageable: A messageable interface to send the embed to
+        :param message: The error message
+        """
         embed = discord.Embed(
-            title='Error', description=reason, color=cls.ERROR_COLOR)
+            title='Error', description=message, color=cls.ERROR_COLOR)
         await messageable.send(embed=embed)
