e832f98a10eb044df3498b15a51407659ab13c22
Hawkpath <hawkpathas@gmail.com>
2020-10-10 17:21:28 -0400

    UserData is now only in control of the database. Added method get_database.

--- sandpiper/user_info/__init__.py
+++ sandpiper/user_info/__init__.py
@@ -1,8 +1,9 @@
 from pathlib import Path
+from typing import Optional
 
 from discord.ext.commands import Bot
 
-from .cog import UserData
+from .cog import UserData, DatabaseUnavailable
 from .database_sqlite import DatabaseSQLite
 
 DB_FILE = Path(__file__).parent.parent / 'sandpiper.db'
@@ -16,6 +17,9 @@ def setup(bot: Bot):
 
 def teardown(bot: Bot):
     """Disconnects from the database"""
-    user_data: UserData = bot.get_cog('UserData')
-    if user_data.database:
-        user_data.database.disconnect()
+    user_data: Optional[UserData] = bot.get_cog('UserData')
+    if user_data is not None:
+        try:
+            user_data.get_database().disconnect()
+        except DatabaseUnavailable:
+            pass
--- sandpiper/user_info/cog.py
+++ sandpiper/user_info/cog.py
@@ -1,211 +1,38 @@
-from datetime import date
 import logging
-from typing import Any, Optional
 
-import discord
 import discord.ext.commands as commands
-from discord.ext.commands import BadArgument
 
-from .enums import PrivacyType
-from ..common.embeds import Embeds
-from .database import Database, DatabaseError
+from .database import Database
 
-__all__ = ['UserData']
+__all__ = ['UserData', 'DatabaseUnavailable']
 
 logger = logging.getLogger('sandpiper.user_info')
 
 
-class DatabaseUnavailable(commands.CheckFailure):
+class DatabaseUnavailable(Exception):
     pass
-
-
-def is_database_available():
-    async def predicate(ctx: commands.Context):
-        cog: Optional[UserData] = ctx.cog
-        if cog is None:
-            logger.error(
-                f'Command has no associated cog (content={ctx.message.content} '
-                f'msg={ctx.message} command={ctx.command})')
-            raise commands.CheckFailure('Unexpected error.')
-        elif not cog.database.connected():
-            raise DatabaseUnavailable()
-        return True
-    return commands.check(predicate)
-
-
-def date_handler(date_str: str):
-    try:
-        return date.fromisoformat(date_str)
-    except ValueError:
-        raise commands.BadArgument(
-            'Use date format YYYY-MM-DD (example: 1997-08-27)')
-
-
-privacy_emojis = {
-    PrivacyType.PRIVATE: '⛔',
-    PrivacyType.PUBLIC: '✅'
-}
-
-
-def user_info_str(field_name: str, value: Any, privacy: PrivacyType):
-    privacy_emoji = privacy_emojis[privacy]
-    privacy = privacy.name.capitalize()
-    return f'{privacy_emoji} `{privacy:7}` | **{field_name}** • {value}'
 
 
 class UserData(commands.Cog):
 
-    database: Database = None
+    _database: Database = None
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
     def set_database_adapter(self, database: Database):
-        self.database = database
+        self._database = database
 
-    @commands.Cog.listener()
-    async def on_command_error(self, ctx: commands.Context,
-                               error: commands.CommandError):
-        if isinstance(error, commands.CommandInvokeError):
-            if isinstance(error.original, DatabaseUnavailable):
-                await Embeds.error(ctx, 'Unable to access database.')
-            elif isinstance(error.original, DatabaseError):
-                await Embeds.error(ctx, 'Error during database operation.')
-            else:
-                logger.error(
-                    f'Unexpected error (content={ctx.message.content!r} '
-                    f'message={ctx.message})', exc_info=error.original)
-                await Embeds.error(ctx, 'Unexpected error')
-        else:
-            await Embeds.error(ctx, str(error))
-
-    @commands.group()
-    async def bio(self, ctx: commands.Context):
-        """Commands for managing your personal info."""
-        pass
-
-    @bio.command(name='clear')
-    @is_database_available()
-    @commands.dm_only()
-    async def bio_clear(self, ctx: commands.Context):
-        """Delete all of your personal info."""
-        user_id: int = ctx.author.id
-        self.database.clear_data(user_id)
-
-    @bio.group(name='show', invoke_without_command=True)
-    @is_database_available()
-    @commands.dm_only()
-    async def bio_show(self, ctx: commands.Context):
+    def get_database(self):
         """
-        Commands for displaying your personal info. Can be run on its own to
-        display all stored info.
+        Get the database adapter for manipulating user data.
+
+        :returns: The database adapter that has been set with
+            ``set_database_adapter``
+        :raises DatabaseUnavailable: if database is not set or connected
         """
-
-        user_id: int = ctx.author.id
-
-        preferred_name = self.database.get_preferred_name(user_id)
-        pronouns = self.database.get_pronouns(user_id)
-        birthday = self.database.get_birthday(user_id)
-        age = self.database.get_age(user_id)
-        age = age if age is not None else 'N/A'
-        timezone = self.database.get_timezone(user_id)
-
-        p_preferred_name = self.database.get_privacy_preferred_name(user_id)
-        p_pronouns = self.database.get_privacy_pronouns(user_id)
-        p_birthday = self.database.get_privacy_birthday(user_id)
-        p_age = self.database.get_privacy_age(user_id)
-        p_timezone = self.database.get_privacy_timezone(user_id)
-
-        await Embeds.info(
-            ctx,
-            f"{user_info_str('Name', preferred_name, p_preferred_name)}\n"
-            f"{user_info_str('Pronouns', pronouns, p_pronouns)}\n"
-            f"{user_info_str('Birthday', birthday, p_birthday)}\n"
-            f"{user_info_str('Age', age, p_age)}\n"
-            f"{user_info_str('Timezone', timezone, p_timezone)}\n"
-        )
-
-    @bio.group(name='set', invoke_without_command=False)
-    async def bio_set(self, ctx: commands.Context):
-        """Commands for setting your personal info."""
-        pass
-
-    # Name
-
-    @bio_show.command(name='name')
-    @is_database_available()
-    @commands.dm_only()
-    async def bio_show_name(self, ctx: commands.Context):
-        """Display your preferred name."""
-        user_id: int = ctx.author.id
-        preferred_name = self.database.get_preferred_name(user_id)
-        privacy = self.database.get_privacy_preferred_name(user_id)
-        await Embeds.info(
-            ctx, user_info_str('Name', preferred_name, privacy))
-
-    @bio_set.command(name='name')
-    @is_database_available()
-    @commands.dm_only()
-    async def bio_set_name(self, ctx: commands.Context, new_name: str):
-        """Set your preferred name."""
-        user_id: int = ctx.author.id
-        if len(new_name) > 64:
-            raise BadArgument(f'Name must be 64 characters or less '
-                              f'(yours: {len(new_name)}).')
-        self.database.set_preferred_name(user_id, new_name)
-        await Embeds.success(ctx, 'Name set!')
-
-    # Pronouns
-
-    @bio_show.command(name='pronouns')
-    @is_database_available()
-    @commands.dm_only()
-    async def bio_show_pronouns(self, ctx: commands.Context):
-        """Display your pronouns."""
-        user_id: int = ctx.author.id
-        pronouns = self.database.get_pronouns(user_id)
-        privacy = self.database.get_privacy_pronouns(user_id)
-        await Embeds.info(
-            ctx, user_info_str('Pronouns', pronouns, privacy))
-
-    @bio_set.command(name='pronouns')
-    @is_database_available()
-    @commands.dm_only()
-    async def bio_set_pronouns(self, ctx: commands.Context, new_pronouns: str):
-        """Set your pronouns."""
-        user_id: int = ctx.author.id
-        if len(new_pronouns) > 64:
-            raise BadArgument(f'Pronouns must be 64 characters or less '
-                              f'(yours: {len(new_pronouns)}).')
-        self.database.set_pronouns(user_id, new_pronouns)
-        await Embeds.success(ctx, 'Pronouns set!')
-
-    # Birthday
-
-    @bio_show.command(name='birthday')
-    @is_database_available()
-    @commands.dm_only()
-    async def bio_show_birthday(self, ctx: commands.Context):
-        """Display your birthday."""
-        user_id: int = ctx.author.id
-        birthday = str(self.database.get_birthday(user_id))
-        privacy = self.database.get_privacy_birthday(user_id)
-        await Embeds.info(
-            ctx, user_info_str('Birthday', birthday, privacy))
-
-    @bio_set.command(name='birthday')
-    @is_database_available()
-    @commands.dm_only()
-    async def bio_set_birthday(self, ctx: commands.Context,
-                               new_birthday: date_handler):
-        """Set your birthday."""
-        user_id: int = ctx.author.id
-        self.database.set_birthday(user_id, new_birthday)
-        await Embeds.success(ctx, 'Birthday set!')
-
-    # Other
-
-    @commands.command(name='who is')
-    @is_database_available()
-    async def who_is(self, ctx: commands.Context, user: discord.Member):
-        pass
+        if self._database is None:
+            raise DatabaseUnavailable('Database adapter is not set.')
+        if not self._database.connected():
+            raise DatabaseUnavailable('Database is not connected.')
+        return self._database
