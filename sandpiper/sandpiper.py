6a214215101dbf04b6bf2965d67063d4df8198de
Hawkpath <hawkpathas@gmail.com>
2020-10-10 18:14:39 -0400

    Added logging for connection-based and error event handlers.

--- sandpiper/sandpiper.py
+++ sandpiper/sandpiper.py
@@ -17,5 +17,17 @@ class Sandpiper(Bot):
         )
         self.load_extension('sandpiper.unit_conversion')
 
+    async def on_connect(self):
+        logger.info('Client connected')
+
+    async def on_disconnect(self):
+        logger.info('Client disconnected')
+
+    async def on_resumed(self):
+        logger.info('Session resumed')
+
     async def on_ready(self):
-        logger.info('Sandpiper client started')
+        logger.info('Client started')
+
+    async def on_error(self, event: str, *args, **kwargs):
+        logger.error(f'Error in event {event}', exc_info=True)
