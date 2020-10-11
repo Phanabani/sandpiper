dceea2ca05582e4b0a48b6bca8145a7e26a60be0
Hawkpath <hawkpathas@gmail.com>
2020-10-10 20:14:56 -0400

    Updated info method to allow an iterable of strings for the message parameter (to be joined with newlines).

--- sandpiper/common/embeds.py
+++ sandpiper/common/embeds.py
@@ -1,4 +1,4 @@
-from typing import Iterable, Tuple
+from typing import Iterable, Tuple, Union
 
 import discord
 
@@ -13,21 +13,36 @@ class Embeds:
 
     @classmethod
     async def info(cls, messageable: discord.abc.Messageable,
-                   message: str = None, fields: Iterable[FieldType] = None):
+                   message: Union[str, Iterable[str]] = None,
+                   fields: Iterable[FieldType] = None):
         """
         Sends an info embed. At least one of ``message`` or ``fields`` must be
         supplied.
 
         :param messageable: A messageable interface to send the embed to
-        :param message: An info message
+        :param message: An info message. Can be either a single string or an
+            iterable of strings to join with newlines. Optional if ``fields``
+            is supplied.
         :param fields: An iterable of fields to add to the embed where each
-            field is a tuple of shape (name, value, inline)
+            field is a tuple of shape (name, value, inline). Optional if
+            ``message`` is supplied.
         """
         if not (message or fields):
             raise ValueError('You must specify at least one of message or fields')
-        embed = discord.Embed(title='Info',
-                              description=message or discord.Embed.Empty,
+
+        if isinstance(message, str):
+            pass
+        elif isinstance(message, Iterable):
+            message = '\n'.join(message)
+        elif message is None:
+            message = discord.Embed.Empty
+        else:
+            raise ValueError('Message must either be None, a single string, '
+                             'or an iterable of strings')
+
+        embed = discord.Embed(title='Info', description=message,
                               color=cls.INFO_COLOR)
+
         if fields:
             for name, value, inline in fields:
                 embed.add_field(name=name, value=value, inline=inline)
