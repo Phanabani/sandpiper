f9d0ab86238a2c554d70473ba1eac67bc92bedc2
Hawkpath <hawkpathas@gmail.com>
2020-10-11 21:35:07 -0400

    Added sep parameter to join function.

--- sandpiper/common/misc.py
+++ sandpiper/common/misc.py
@@ -1,11 +1,11 @@
 from typing import Iterable
 
-__all__ = ['prune']
+__all__ = ['join', 'prune']
+
+
+def join(*fragments, sep=''):
+    return sep.join(str(f) for f in fragments if f)
 
 
 def prune(iterable: Iterable):
     return (i for i in iterable if i)
-
-
-def join(*fragments):
-    return ''.join(str(f) for f in fragments if f)
