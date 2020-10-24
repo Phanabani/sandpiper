04c0db3e0e3325120ac1be6c175341c26d067c0f
Hawkpath <hawkpathas@gmail.com>
2020-10-24 19:20:14 -0400

    Added utc_now function.

--- sandpiper/common/time.py
+++ sandpiper/common/time.py
@@ -1,7 +1,16 @@
-from typing import Union
+import datetime as dt
+from typing import Union, cast
 
 import pytz
 
-__all__ = ['TimezoneType']
+__all__ = ['TimezoneType', 'utc_now']
+
+import tzlocal
 
 TimezoneType = Union[pytz.tzinfo.StaticTzInfo, pytz.tzinfo.DstTzInfo]
+
+
+def utc_now() -> dt.datetime:
+    # Get the system-local timezone and use it to localize dt.datetime.now()
+    local_tz = cast(TimezoneType, tzlocal.get_localzone())
+    return local_tz.localize(dt.datetime.now())
