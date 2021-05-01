82ec82d17aefd0dec50cc5606f7992002edb390e
Hawkpath <hawkpathas@gmail.com>
2021-05-01 02:33:57 -0400

    Give ParsingError better output and add field path info.

--- sandpiper/config_parser/exceptions.py
+++ sandpiper/config_parser/exceptions.py
@@ -1,4 +1,4 @@
-from typing import Any, Type
+from typing import *
 
 from .misc import qualified
 
@@ -20,12 +20,21 @@ class MissingFieldError(Exception):
 
 class ParsingError(Exception):
 
-    def __init__(self, value: Any, target_type: Type, base_exc: Exception):
+    def __init__(
+            self, value: Any, target_type: Type, base_exc: Exception,
+            field_parent: str = '', field_name: str = ''
+    ):
         self.value = value
         self.target_type = target_type
         self.base_exc = base_exc
+        self.add_field_info(field_parent, field_name)
+
+    def add_field_info(self, parent: str, name: str):
+        self.field_path = qualified(parent, name)
 
     def __str__(self):
         return (
-            f"Failed to parse {self.value!r} as {self.target_type}"
+            f"Failed to parse config value (path={self.field_path} "
+            f"value={self.value!r} target_type={self.target_type} "
+            f"exc=\"{self.base_exc}\")"
         )
