7116af6c83aa7359f7ad15ff206c97dcbd863905
Hawkpath <hawkpathas@gmail.com>
2021-05-02 06:23:47 -0400

    Add Convert class. Instead of allowing implicit conversions (like log_file: Path), we will now explicitly annotate with Convert[str, Path]. This is to make the json fields strongly typed and disallow a coercion of, for example, '1' -> 1. This also will help when marking a field as a Union, since our previous weakly-typed strategy had no idea of what type to expect of a json field, only of what type it should become.

--- sandpiper/config_parser/converters.py
+++ sandpiper/config_parser/converters.py
@@ -2,32 +2,76 @@ from abc import ABCMeta, abstractmethod
 from typing import *
 
 
-class ConfigConverter(metaclass=ABCMeta):
+class ConfigConverterBase(metaclass=ABCMeta):
 
     @abstractmethod
     def convert(self, value: Any) -> Any:
         pass
 
+    @classmethod
+    def _check_tuple(
+            cls, tuple_: Tuple, **fields: Union[Type, Tuple[Type, ...]]
+    ) -> Tuple:
+        if len(tuple_) != len(fields):
+            raise ValueError(
+                f"Missing type arguments. Expected "
+                f"{cls.__name__}[{', '.join(fields.keys())}]"
+            )
+        for value, name, type_ in zip(tuple_, fields.keys(), fields.values()):
+            cls._typecheck(type_, value, name)
+        return tuple_
 
-class BoundedInt(ConfigConverter):
+    @staticmethod
+    def _typecheck(type_: Type, value: Any, name: str) -> NoReturn:
+        if not isinstance(value, type_):
+            raise TypeError(
+                f"{name}={value} must be "
+                f"{'one ' if isinstance(type_, tuple) else ''}"
+                f"of type {type_}, not {type(value)}"
+            )
+
+
+V_Base = TypeVar('V_Base')
+V_Target = TypeVar('V_Target')
+
+
+class Convert(ConfigConverterBase):
+
+    def __init__(self, base_type: Type[V_Base], target_type: Type[V_Target]):
+        self.base_type = base_type
+        self.target_type = target_type
+
+    def __class_getitem__(
+            cls, base_and_target_types: Tuple[Type[V_Base], Type[V_Target]]
+    ):
+        base_type, target_type = cls._check_tuple(
+            base_and_target_types, base_type=type, target_type=type
+        )
+        return cls(base_type, target_type)
+
+    def convert(self, value: V_Base) -> V_Target:
+        if not isinstance(value, self.base_type):
+            raise TypeError(f'Expected type {self.base_type}, got {type(value)}')
+        return self.target_type(value)
+
+
+# noinspection PyMissingConstructor
+class BoundedInt(int, ConfigConverterBase):
 
     def __init__(self, min: Optional[int], max: Optional[int]):
-        if (not (min is None or isinstance(min, int))
-                or not (max is None or isinstance(max, int))):
-            raise TypeError('min and max must be either integers or None')
-
         self.min = min
         self.max = max
 
     def __class_getitem__(cls, min_max: Tuple[Optional[int], Optional[int]]):
-        if len(min_max) != 2:
-            raise ValueError(
-                f"You must supply a minimum and maximum value, separated by a "
-                f"comma. One or both may be None to specify there is no "
-                f"bound in that direction."
-            )
+        cls._check_tuple(min_max, min=(int, type(None)), max=(int, type(None)))
         return cls(min=min_max[0], max=min_max[1])
 
     def convert(self, value: Any) -> int:
-        pass
+        self._typecheck(int, value, 'value')
+        if self.min is not None and value < self.min:
+            raise ValueError(f"Value must be greater than or equal to {self.min}")
+        if self.max is not None and value > self.max:
+            raise ValueError(f"Value must be less than or equal to {self.max}")
+        return value
+
 
