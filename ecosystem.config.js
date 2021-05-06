9e20025f874feaa09753cb1769d58f2279c4adbe
Hawkpath <hawkpathas@gmail.com>
2021-05-06 01:38:22 -0400

    Fix the Pipenv "workaround" which didn't actually work. And add a very sad, defeated comment. :(

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,28 @@
+// Hey. So I spent a while trying to get this to work on Windows.
+// After migrating to pipenv, it's kept spawning an extra python
+// console window. It was kind of a waste of time because I'm going
+// to be running sandpiper on linux.
+// 
+// However, I found that the console window could be closed
+// while leaving sandpiper running by killing the "conhost" child
+// process of pipenv. So, whoever you are reading this, if you
+// need to run sandpiper on windows without an extra window....
+// just kill conhost. and I'm sorry for any pain this caused. :(
+// I'm not spending any more time on this
+
 module.exports = {
-  'apps': [
-    {
-      'name': 'sandpiper',
-      'interpreter': 'pipenv',
-      'interpreter_args': ['run', 'python', '-m'],
-      'script': 'sandpiper',
-      // script is a required field, so this is a hacky way to make it work
+    apps: [
+        {
+            name: 'sandpiper',
+            script: 'pipenv',
+            args: ['run', 'python', '-m', 'sandpiper'],
 
-      'watch': false,
+            watch: false,
 
-      'min_uptime': '5s',
-      'max_restarts': 1,
-      'restart_delay': 0,
-      'autorestart': true
-    }
-  ]
+            min_uptime: '5s',
+            max_restarts: 3,
+            restart_delay: 0,
+            autorestart: true
+        }
+    ]
 };
