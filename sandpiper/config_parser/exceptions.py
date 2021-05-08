48ac0d84e52cd4ea9812d543edd18bbc6796188b
Hawkpath <hawkpathas@gmail.com>
2021-05-08 19:09:10 -0400

    Remove the fancy raise-at-line thing, it's way too volatile for very little gain.

--- sandpiper/config_parser/exceptions.py
+++ sandpiper/config_parser/exceptions.py
@@ -1,7 +1,4 @@
-import inspect
-import re
-from types import TracebackType
-from typing import Any, Optional
+from typing import Any
 
 __all__ = (
     'ConfigSchemaError', 'ConfigParsingError', 'MissingFieldError',
@@ -9,63 +6,18 @@ __all__ = (
 )
 
 
-class _RaiseAtClassMemberException(Exception):
+class ConfigSchemaError(Exception):
 
-    def __init__(
-            self, cls: type, member_name: str, msg: str
-    ):
+    def __init__(self, cls: type, field_name: str, msg: str):
         self.cls = cls
-        self.member_name = member_name
+        self.field_name = field_name
         self.msg = msg
-
-        line = _find_class_member_line(cls, member_name)
-        if line is not None:
-            tb = TracebackType(
-                tb_next=None, tb_frame=inspect.currentframe(),
-                tb_lasti=0, tb_lineno=line
-            )
-            self.with_traceback(tb)
-
-
-_find_indent_pattern = re.compile(r'([ \t]+)[a-zA-Z]')
-
-
-def _find_class_member_line(cls: type, member_name: str) -> Optional[int]:
-    if not isinstance(cls, type):
-        raise TypeError(f"cls must be a class, got {type(cls)}")
-
-    member_pattern = re.compile(
-        rf'([ \t]+){member_name} *[:=]'
-    )
-
-    first = True
-    indent = None
-    lines, start_line = inspect.getsourcelines(cls)
-    for i, line in enumerate(lines):
-        if first:
-            first = False
-            continue
-        if indent is None and (m := _find_indent_pattern.match(line)):
-            # Store the indentation level for the first expression in
-            # the class body
-            indent = m[1]
-        if (indent is not None
-                and (m := member_pattern.match(line))
-                and m[1] == indent
-        ):
-            # Found the member definition
-            return start_line + i
-    return None
-
-
-class ConfigSchemaError(_RaiseAtClassMemberException):
 
     def __str__(self):
         return (
-            f"Field {self.member_name} in {self.cls.__qualname__} is "
+            f"Field {self.field_name} in {self.cls.__qualname__} is "
             f"invalid: {self.msg}"
         )
-
 
 
 class ConfigParsingError(Exception):
