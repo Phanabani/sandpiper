d956aa5345515934e6367964b123a9ea71715ad5
Hawkpath <hawkpathas@gmail.com>
2021-05-09 04:56:30 -0400

    Add parsing assertions to simple tests.

--- sandpiper/config_parser/test_parser.py
+++ sandpiper/config_parser/test_parser.py
@@ -23,26 +23,49 @@ class TestSimple:
         with not_raises(ConfigSchemaError):
             class C(ConfigCompound):
                 field: None
+        parsed = C('{"field": null}')
+        assert parsed.field is None
+        with pytest.raises(TypeError):
+            parsed = C('{"field": 1}')
 
     def test_bool(self):
         with not_raises(ConfigSchemaError):
             class C(ConfigCompound):
                 field: bool
+        parsed = C('{"field": true}')
+        assert parsed.field is True
+        with pytest.raises(TypeError):
+            parsed = C('{"field": 1}')
 
     def test_int(self):
         with not_raises(ConfigSchemaError):
             class C(ConfigCompound):
                 field: int
+        parsed = C('{"field": 1}')
+        assert isinstance(parsed.field, int)
+        assert parsed.field == 1
+        with pytest.raises(TypeError):
+            parsed = C('{"field": "str"}')
 
     def test_float(self):
         with not_raises(ConfigSchemaError):
             class C(ConfigCompound):
                 field: float
+        parsed = C('{"field": 1.0}')
+        assert isinstance(parsed.field, float)
+        assert parsed.field == 1.0
+        with pytest.raises(TypeError):
+            parsed = C('{"field": "str"}')
 
     def test_str(self):
         with not_raises(ConfigSchemaError):
             class C(ConfigCompound):
                 field: str
+        parsed = C('{"field": "str"}')
+        assert isinstance(parsed.field, str)
+        assert parsed.field == 'str'
+        with pytest.raises(TypeError):
+            parsed = C('{"field": 1}')
 
     def test_invalid(self):
         with pytest.raises(ConfigSchemaError):
