1c073ddb343384941b3c68268d0245b56efd262a
Hawkpath <hawkpathas@gmail.com>
2021-05-11 03:00:54 -0400

    Split transformers tests into separate classes.

--- sandpiper/config_parser/test_transformers.py
+++ sandpiper/config_parser/test_transformers.py
@@ -13,9 +13,9 @@ def assert_type_value(value, assert_type: type, assert_value):
     assert value == assert_value
 
 
-class TestAnnotations:
+class TestFromType:
 
-    def test_fromtype_implicit_to(self):
+    def test_implicit_to(self):
         class C(ConfigCompound):
             field: A[str, FromType(int)]
 
@@ -25,31 +25,31 @@ class TestAnnotations:
         with pytest.raises(TypeError):
             C('{"field": "123"}')
 
-    def test_fromtype_multiple_implicit_to_type_err(self):
+    def test_multiple_implicit_to_type_err(self):
         with pytest.raises(ConfigSchemaError, match='implicit'):
             class C(ConfigCompound):
                 field: A[str, FromType(int), FromType(str)]
 
-    def test_fromtype_explicit_to(self):
+    def test_explicit_to(self):
         class C(ConfigCompound):
             field: A[str, FromType(int, str)]
 
         parsed = C('{"field": 123}')
         assert_type_value(parsed.field, str, "123")
 
-    def test_fromtype_explicit_to_chained(self):
+    def test_explicit_to_chained(self):
         class C(ConfigCompound):
             field: A[str, FromType(int, float), FromType(float, str)]
 
         parsed = C('{"field": 123}')
         assert_type_value(parsed.field, str, "123.0")
 
-    def test_fromtype_explicit_to_type_mismatch(self):
+    def test_explicit_to_type_mismatch(self):
         with pytest.raises(ConfigSchemaError):
             class C(ConfigCompound):
                 field: A[str, FromType(int, float)]
 
-    def test_fromtype_implicit_with_bounded_after(self):
+    def test_implicit_with_bounded_after(self):
         # I want the shorthand implicit notation available if it's used at
         # the last FromType, however I think it's difficult to understand
         # what's going on if more transformers come after it
@@ -57,7 +57,7 @@ class TestAnnotations:
             class C(ConfigCompound):
                 field: A[int, FromType(str), Bounded(2, 4)]
 
-    def test_fromtype_explicit_with_bounded_after(self):
+    def test_explicit_with_bounded_after(self):
         class C(ConfigCompound):
             field: A[int, FromType(str, int), Bounded(2, 4)]
 
@@ -67,7 +67,10 @@ class TestAnnotations:
         with pytest.raises(ValueError):
             parsed = C('{"field": "5"}')
 
-    def test_bounded_min(self):
+
+class TestBounded:
+
+    def test_min(self):
         class C(ConfigCompound):
             field: A[int, Bounded(5, None)]
 
@@ -80,7 +83,7 @@ class TestAnnotations:
         parsed = C('{"field": 6}')
         assert_type_value(parsed.field, int, 6)
 
-    def test_bounded_max(self):
+    def test_max(self):
         class C(ConfigCompound):
             field: A[int, Bounded(None, 10)]
 
@@ -93,7 +96,7 @@ class TestAnnotations:
         with pytest.raises(ValueError):
             C('{"field": 11}')
 
-    def test_bounded_min_max(self):
+    def test_min_max(self):
         class C(ConfigCompound):
             field: A[int, Bounded(2, 4)]
 
@@ -112,7 +115,7 @@ class TestAnnotations:
         with pytest.raises(ValueError):
             C('{"field": 5}')
 
-    def test_bounded_min_equal_to_max(self):
+    def test_min_equal_to_max(self):
         class C(ConfigCompound):
             field: A[int, Bounded(2, 2)]
 
@@ -125,7 +128,7 @@ class TestAnnotations:
         with pytest.raises(ValueError):
             C('{"field": 3}')
 
-    def test_bounded_min_greater_than_max(self):
+    def test_min_greater_than_max(self):
         with pytest.raises(ValueError):
             class C(ConfigCompound):
                 field: A[int, Bounded(2, 1)]
