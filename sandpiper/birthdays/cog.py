d3c211e85b91044601d2460b61d8f580a8853f65
Hawkpath <hawkpathas@gmail.com>
2021-05-29 01:11:51 -0400

    Extract schedule_birthday to its own method.

--- sandpiper/birthdays/cog.py
+++ sandpiper/birthdays/cog.py
@@ -42,32 +42,44 @@ class Birthdays(commands.Cog):
         db = await self._get_database()
         now = utc_now()
         today = now.date()
+
         for user_id, birthday in await db.get_birthdays_range(
                 today, today + dt.timedelta(days=1)
         ):
-            timezone = None
-            if await db.get_privacy_timezone(user_id) is PrivacyType.PUBLIC:
-                timezone = await db.get_timezone(user_id)
-            if timezone is None:
-                # If the user's timezone is null, just use UTC
-                timezone = pytz.UTC
+            await self.schedule_birthday(user_id, birthday, now=now)
 
-            # Determine midnight in this person's timezone so we can
-            # wish them happy birthday at the start of their day
-            midnight_local: dt.datetime = timezone.localize(
-                dt.datetime(today.year, birthday.month, birthday.day)
+    async def schedule_birthday(
+            self, user_id: int, birthday: dt.date,
+            *, now: Optional[dt.datetime] = None
+    ):
+        db = await self._get_database()
+
+        if now is None:
+            now = utc_now()
+        today = now.date()
+
+        timezone = None
+        if await db.get_privacy_timezone(user_id) is PrivacyType.PUBLIC:
+            timezone = await db.get_timezone(user_id)
+        if timezone is None:
+            # If the user's timezone is null, just use UTC
+            timezone = pytz.UTC
+
+        # Determine midnight in this person's timezone so we can
+        # wish them happy birthday at the start of their day
+        midnight_local: dt.datetime = timezone.localize(
+            dt.datetime(today.year, birthday.month, birthday.day)
+        )
+        midnight_utc = midnight_local.astimezone(pytz.UTC)
+        midnight_delta = midnight_utc - now
+        # Only schedule the birthday task if their localized midnight is
+        # within 24 hours from now
+        # TODO I'm worried that it could be possible we lose a birthday
+        #   in a race condition here...
+        if dt.timedelta(0) < midnight_delta <= dt.timedelta(hours=24):
+            await self.bot.loop.create_task(
+                self.send_birthday_message(user_id, midnight_delta)
             )
-            midnight_utc = midnight_local.astimezone(pytz.UTC)
-            midnight_delta = midnight_utc - now
-            # Only schedule the birthday task if their localized midnight is
-            # within 24 hours from now
-            # TODO I'm worried that it could be possible we lose a birthday
-            #   in a race condition here...
-            if (midnight_delta > dt.timedelta(0)
-                    and midnight_delta <= dt.timedelta(hours=24)):
-                await self.bot.loop.create_task(
-                    self.send_birthday_message(user_id, midnight_delta)
-                )
 
     async def send_birthday_message(self, user_id: int, delta: dt.timedelta):
         await asyncio.sleep(delta.total_seconds())
