52efa313f79be51b740fa9b4e352a1c0358ce20a
Hawkpath <hawkpathas@gmail.com>
2021-06-19 03:34:13 -0400

    Configure Alembic.

--- sandpiper/user_data/alembic/env.py
+++ sandpiper/user_data/alembic/env.py
@@ -1,9 +1,15 @@
 from logging.config import fileConfig
+from pathlib import Path
+import sys
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+
+sandpiper_root_dir = Path(__file__, '../../../..')
+sys.path.insert(0, str(sandpiper_root_dir.absolute()))
+from sandpiper.user_data.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -17,7 +23,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
