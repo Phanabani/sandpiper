05dd6900f26df9e24ae4ec96f00146a269638c3f
Hawkpath <hawkpathas@gmail.com>
2021-06-30 19:14:30 -0400

    Add assert_count_equal for unordered comparison of iterables.

--- sandpiper/tests/_helpers.py
+++ sandpiper/tests/_helpers.py
@@ -1,19 +1,15 @@
+from collections import Counter
+from collections.abc import Iterable
 import re
 from typing import NoReturn, TypeVar, Union
 
-__all__ = (
-    'assert_one_if_list', 'assert_in', 'assert_regex'
-)
+__all__ = [
+    'assert_in', 'assert_regex',
+    'assert_one_if_list',
+    'assert_count_equal',
+]
 
 V = TypeVar('V')
-
-
-def assert_one_if_list(x: Union[list[V], V]) -> V:
-    __tracebackhide__ = True
-    if isinstance(x, list):
-        assert len(x) == 1, f"Expected only one item in list, got {len(x)}"
-        return x[0]
-    return x
 
 
 def assert_in(str_: str, *substrings: str) -> NoReturn:
@@ -38,4 +34,23 @@ def assert_regex(str_: str, *patterns: str) -> NoReturn:
         )
 
 
-# endregion
+def assert_one_if_list(x: Union[list[V], V]) -> V:
+    """
+    Assert, if `x` is a list, that x has only one element and return that
+    element, else return `x` as is.
+    """
+    __tracebackhide__ = True
+    if isinstance(x, list):
+        assert len(x) == 1, f"Expected only one item in list, got {len(x)}"
+        return x[0]
+    return x
+
+
+def assert_count_equal(it_a: Iterable, it_b: Iterable) -> NoReturn:
+    """
+    Assert that both iterables have the same elements, regardless of order.
+    Inspired by unittest.TestCase.assertCountEqual.
+    """
+    count_a = Counter(it_a)
+    count_b = Counter(it_b)
+    assert count_a == count_b
