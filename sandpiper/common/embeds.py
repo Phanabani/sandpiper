c88a925c2767cefdc0fb87142a6e5944f843d19a
Hawkpath <hawkpathas@gmail.com>
2021-07-21 05:40:32 -0400

    Add special embed.

--- sandpiper/common/embeds.py
+++ sandpiper/common/embeds.py
@@ -16,11 +16,14 @@ class Embeds:
     SUCCESS_COLOR = 0x57FCA5
     WARNING_COLOR = 0xE7D900
     ERROR_COLOR = 0xFF0000
+    SPECIAL_COLOR = 0xF656F1
 
     @classmethod
-    async def info(cls, messageable: discord.abc.Messageable,
-                   message: Union[str, Iterable[str]] = None,
-                   fields: Iterable[FieldType] = None):
+    async def info(
+            cls, messageable: discord.abc.Messageable,
+            message: Union[str, Iterable[str]] = None,
+            fields: Iterable[FieldType] = None
+    ):
         """
         Sends an info embed. At least one of ``message`` or ``fields`` must be
         supplied.
@@ -34,7 +37,9 @@ class Embeds:
             ``message`` is supplied.
         """
         if not (message or fields):
-            raise ValueError('You must specify at least one of message or fields')
+            raise ValueError(
+                "You must specify at least one of message or fields"
+            )
 
         if isinstance(message, str):
             pass
@@ -43,11 +48,14 @@ class Embeds:
         elif message is None:
             message = discord.Embed.Empty
         else:
-            raise ValueError('Message must either be None, a single string, '
-                             'or an iterable of strings')
+            raise ValueError(
+                "Message must either be None, a single string, or an iterable "
+                "of strings"
+            )
 
-        embed = discord.Embed(title='Info', description=message,
-                              color=cls.INFO_COLOR)
+        embed = discord.Embed(
+            title='Info', description=message, color=cls.INFO_COLOR
+        )
 
         if fields:
             for name, value, inline in fields:
@@ -55,8 +63,10 @@ class Embeds:
         await messageable.send(embed=embed)
 
     @classmethod
-    async def success(cls, messageable: discord.abc.Messageable,
-                      message: Union[str, Iterable[str]]):
+    async def success(
+            cls, messageable: discord.abc.Messageable,
+            message: Union[str, Iterable[str]]
+    ):
         """
         Sends a success embed.
 
@@ -67,12 +77,15 @@ class Embeds:
         if not isinstance(message, str) and isinstance(message, Iterable):
             message = join(*message, sep='\n')
         embed = discord.Embed(
-            title='Success', description=message, color=cls.SUCCESS_COLOR)
+            title='Success', description=message, color=cls.SUCCESS_COLOR
+        )
         await messageable.send(embed=embed)
 
     @classmethod
-    async def warning(cls, messageable: discord.abc.Messageable,
-                      message: Union[str, Iterable[str]]):
+    async def warning(
+            cls, messageable: discord.abc.Messageable,
+            message: Union[str, Iterable[str]]
+    ):
         """
         Sends a success embed.
 
@@ -83,12 +96,15 @@ class Embeds:
         if not isinstance(message, str) and isinstance(message, Iterable):
             message = join(*message, sep='\n')
         embed = discord.Embed(
-            title='Warning', description=message, color=cls.WARNING_COLOR)
+            title='Warning', description=message, color=cls.WARNING_COLOR
+        )
         await messageable.send(embed=embed)
 
     @classmethod
-    async def error(cls, messageable: discord.abc.Messageable,
-                    message: Union[str, Iterable[str]]):
+    async def error(
+            cls, messageable: discord.abc.Messageable,
+            message: Union[str, Iterable[str]]
+    ):
         """
         Sends an error embed.
 
@@ -99,5 +115,26 @@ class Embeds:
         if not isinstance(message, str) and isinstance(message, Iterable):
             message = join(*message, sep='\n')
         embed = discord.Embed(
-            title='Error', description=message, color=cls.ERROR_COLOR)
+            title='Error', description=message, color=cls.ERROR_COLOR
+        )
+        await messageable.send(embed=embed)
+
+    @classmethod
+    async def special(
+            cls, messageable: discord.abc.Messageable, title: str,
+            message: Union[str, Iterable[str]]
+    ):
+        """
+        Sends a special embed (such as for announcements).
+
+        :param messageable: A messageable interface to send the embed to
+        :param title: The embed's title
+        :param message: The special message. Can be either a single string or an
+            iterable of strings to join with newlines.
+        """
+        if not isinstance(message, str) and isinstance(message, Iterable):
+            message = join(*message, sep='\n')
+        embed = discord.Embed(
+            title=title, description=message, color=cls.SPECIAL_COLOR
+        )
         await messageable.send(embed=embed)
