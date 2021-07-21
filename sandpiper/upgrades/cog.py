132158172a7301dbcc72ed7772d8dac2f0aa2dd2
Hawkpath <hawkpathas@gmail.com>
2021-07-21 03:02:41 -0400

    Various fixes.

--- sandpiper/upgrades/cog.py
+++ sandpiper/upgrades/cog.py
@@ -10,7 +10,6 @@ from sandpiper.user_data import UserData
 
 __all__ = ['Upgrades']
 
-
 logger = logging.getLogger(__name__)
 
 
@@ -19,15 +18,21 @@ class Upgrades(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    def do_upgrades(self):
+    @commands.Cog.listener('on_ready')
+    async def do_upgrades(self):
         user_data: Optional[UserData] = self.bot.get_cog('UserData')
         if user_data is None:
             logger.warning(
-                f"Failed to get the UserData cog. Skipping upgrade handlers."
+                f"Failed to get the UserData cog; skipping upgrade handlers."
             )
+            return
+
         db = await user_data.get_database()
         await db.ready()
         previous_version = await db.get_sandpiper_version()
+
         await do_upgrades(
             self.bot, previous_version, current_version, all_upgrade_handlers
         )
+
+        await db.set_sandpiper_version(current_version)
