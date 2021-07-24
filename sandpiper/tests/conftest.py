4f95d35d6b39224e2b2b0e787965228287bde67b
Hawkpath <hawkpathas@gmail.com>
2021-07-23 23:05:31 -0400

    Move patch_all_symbol_imports to _helpers.

--- sandpiper/tests/conftest.py
+++ sandpiper/tests/conftest.py
@@ -1,15 +1,15 @@
 import datetime as dt
-import sys
+from typing import Optional
 import unittest.mock as mock
-from typing import Any, Optional
 
 import discord
 import discord.ext.commands as commands
 import pytest
 import pytz
 
-from ._discord_helpers import *
 from sandpiper.user_data import DatabaseSQLite
+from ._helpers import patch_all_symbol_imports
+from ._discord_helpers import *
 
 
 # region Discord arrange fixtures
@@ -340,70 +340,6 @@ async def database() -> DatabaseSQLite:
     patcher.stop()
 
 
-def __patch_all_symbol_imports(
-        target_symbol: Any, match_prefix: Optional[str] = None,
-        skip_substring: Optional[str] = None
-):
-    """
-    Iterate through every visible module (in sys.modules) that starts with
-    `match_prefix` to find imports of `target_symbol` and return a list
-    of patchers for each import.
-
-    This is helpful when you want to patch a module, function, or object
-    everywhere in your project's code, even when it is imported with an alias.
-
-    Example:
-
-    ::
-
-        import datetime
-
-        # Setup
-        patchers = __patch_all_symbol_imports(datetime, 'my_project.', 'test')
-        for patcher in patchers:
-            mock_dt = patcher.start()
-            # Do stuff with the mock
-
-        # Teardown
-        for patcher in patchers:
-            patcher.stop()
-
-    :param target_symbol: the symbol to search for imports of (may be a module,
-        a function, or some other object)
-    :param match_prefix: if not None, only search for imports in
-        modules that begin with this string
-    :param skip_substring: if not None, skip any module that contains this
-        substring (e.g. 'test' to skip unit test modules)
-    :return: a list of patchers for each import of the target symbol
-    """
-
-    patchers = []
-
-    # Iterate through all currently imported modules
-    # Make a copy in case it changes
-    for module in list(sys.modules.values()):
-        name_matches = (
-                match_prefix is None
-                or module.__name__.startswith(match_prefix)
-        )
-        should_skip = (
-            skip_substring is not None and skip_substring in module.__name__
-        )
-        if not name_matches or should_skip:
-            continue
-
-        # Iterate through this module's locals
-        # Again, make a copy
-        for local_name, local in list(module.__dict__.items()):
-            if local is target_symbol:
-                # Patch this symbol local to the module
-                patchers.append(mock.patch(
-                    f'{module.__name__}.{local_name}', autospec=True
-                ))
-
-    return patchers
-
-
 @pytest.fixture()
 def patch_localzone_utc() -> pytz.UTC:
     # Patch localzone to use UTC
@@ -424,7 +360,7 @@ def patch_datetime():
 
     def f(static_datetime: dt.datetime) -> dt.datetime:
         # Patch datetime to use a static datetime
-        for patcher in __patch_all_symbol_imports(dt, 'sandpiper.', 'test'):
+        for patcher in patch_all_symbol_imports(dt, 'sandpiper.', 'test'):
             patchers.append(patcher)
             mock_datetime = patcher.start()
 
