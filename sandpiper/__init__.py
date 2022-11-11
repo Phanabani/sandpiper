1e9b3b8cddd2fa66d4e15d5b298d915444450edb
Phanabani <phanabani@gmail.com>
2022-11-11 01:23:47 -0500

    Remove certifi.

--- sandpiper/__init__.py
+++ sandpiper/__init__.py
@@ -1,12 +1,3 @@
-import sys
-
-if sys.platform == "win32":
-    # Temporary fix for expired certificates on Windows
-    import certifi
-    import os
-
-    os.environ["SSL_CERT_FILE"] = certifi.where()
-
 import logging
 
 logger = logging.getLogger(__name__)
