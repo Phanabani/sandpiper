115c65f59ac8424b4456ff9173d5741953dd3512
Phanabani <phanabani@gmail.com>
2022-12-03 19:54:30 -0500

    Dump YAML instead of JSON.

--- sandpiper/config/__main__.py
+++ sandpiper/config/__main__.py
@@ -1,21 +1,21 @@
-import json
 from pathlib import Path
+
+import yaml
 
 from sandpiper.config import SandpiperConfig
 
+YAML_STR_SCALAR_TAG = "tag:yaml.org,2002:str"
 
-def serialize_path(p: Path):
-    return str(p)
+dumper = yaml.SafeDumper
 
 
-class CustomEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, Path):
-            return serialize_path(obj)
-        # Let the base class default method raise the TypeError
-        return json.JSONEncoder.default(self, obj)
+def serialize_path(dumper: yaml.BaseDumper, p: Path):
+    return dumper.represent_scalar(YAML_STR_SCALAR_TAG, str(p))
+
+
+dumper.add_multi_representer(Path, serialize_path)
 
 
 if __name__ == "__main__":
     config = SandpiperConfig(bot_token="YOUR_BOT_TOKEN")
-    print(json.dumps(config.dict(), cls=CustomEncoder, indent=2))
+    print(yaml.dump(config.dict(), Dumper=dumper, indent=2, sort_keys=False))
