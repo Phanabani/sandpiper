04b392b96a433173c0b6eeed1a91c963db1a967f
Phanabani <phanabani@gmail.com>
2022-12-08 01:15:08 -0500

    Make `setup` and `teardown` methods optional.

--- sandpiper/common/component.py
+++ sandpiper/common/component.py
@@ -9,10 +9,8 @@ class Component(abc.ABC):
     def __init__(self, sandpiper: Sandpiper):
         self.sandpiper = sandpiper
 
-    @abc.abstractmethod
     async def setup(self):
         pass
 
-    @abc.abstractmethod
     async def teardown(self):
         pass
