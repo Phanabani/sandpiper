d95bc0cc90d8e852b04959f996a2e4092b3a8a5b
Phanabani <phanabani@gmail.com>
2023-02-10 23:09:02 -0500

    Ignore import not at top of file.

--- sandpiper/components/user_data/alembic/env.py
+++ sandpiper/components/user_data/alembic/env.py
@@ -9,7 +9,7 @@ from sqlalchemy.ext.asyncio import AsyncEngine
 
 sandpiper_root_dir = Path(__file__, "../../../../..")
 sys.path.insert(0, str(sandpiper_root_dir.absolute()))
-from sandpiper.components.user_data.models import Base
+from sandpiper.components.user_data.models import Base  # noqa
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
