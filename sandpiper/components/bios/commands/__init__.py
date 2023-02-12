42502e9ef61562c64c4f806436db55ecc4ceb5c6
Phanabani <phanabani@gmail.com>
2023-02-12 13:14:55 -0500

    Add `misc` command package.

--- sandpiper/components/bios/commands/__init__.py
+++ sandpiper/components/bios/commands/__init__.py
@@ -2,6 +2,7 @@ from .age import age_group
 from .bios import bios_group
 from .birthday import birthday_group
 from .birthday_channel import birthday_channel_group
+from .misc import *
 from .name import name_group
 from .privacy import privacy_group
 from .pronouns import pronouns_group
